022591e2c5a10d913be96399c350bc412975cc0e
giuliacajati <giuliacajati@gmail.com>
2021-04-29 12:57:18 -0400

    modifications tomarkers

--- src/Map.js
+++ src/Map.js
@@ -1,34 +1,50 @@
-import { MapContainer, TileLayer, Marker, Popup, FeatureGroup, Circle, withLeaflet, useMap } from 'react-leaflet'
 import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, FeatureGroup, Circle, useMap, Polyline } from 'react-leaflet'
 import { Icon } from 'leaflet';
 import { EditControl } from 'react-leaflet-draw';
+import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
 import 'leaflet-geosearch/dist/geosearch.css';
 
-
-import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
-
-
-
+// Next steps
+// 1. Show polyline for each marker
+    // Make addPolyline method (API request to OSRM)
+    // a. save polyline to a variable (each marker can store polyline that points to prev marker) 
+    // b. on pageload addPolyline for each marker 
+    
+// 2. Make New Drawing: 
+    // a. Add Marker ... addPolyline
+    // b. Edit Marker
+    // c. Delete Marker 
+// 3. Option to show all images or select from list 
 
 const Map = () => {
     const [data, setData] = useState()
+    
+    const returnPolylines = (marker) => {
+        return marker.add_polyline.forEach(coordinates => {
+            return coordinates + ','
+        })
+    }
 
+    
+    
     const runner = new Icon({
         iconUrl:'https://upload.wikimedia.org/wikipedia/commons/b/b0/Running_icon_-_Noun_Project_17825.svg',
         iconSize: [25, 25]
     })
-
-   
+ 
     useEffect(() => {
       fetch('http://localhost:3000/drawings')
       .then(data => data.json())
       .then(drawings => {
         setData(drawings);
       }) 
-    })
+      
+    },[])
 
     function LeafletgeoSearch() {
         const map = useMap();
+      
         useEffect(() => {
           const provider = new OpenStreetMapProvider();
       
@@ -38,18 +54,13 @@ const Map = () => {
             //     runner
             // }
           });
-      
           map.addControl(searchControl);
-      
           return () => map.removeControl(searchControl);
         }, []);
       
         return null;
       }
-
-    
-
-    
+      
 
 return(
     <div>
@@ -58,8 +69,9 @@ return(
          <div>
              
              <MapContainer center={[38.9072, -77.0369]} zoom={15} stroke={true}>
-           
-            
+    
+
+                <LeafletgeoSearch />
                 <FeatureGroup>
                     <EditControl
                     position='topright'
@@ -70,7 +82,6 @@ return(
                         rectangle: false
                     }}
                     />
-                    
                     <Circle center={[51.51, -0.06]} radius={200} />
                 </FeatureGroup>
                 
@@ -78,20 +89,22 @@ return(
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <LeafletgeoSearch />
-             
                 
+             
                 <div>
- 
-          
-                    {data[0].markers.map(marker => { 
-                        return <Marker 
+                    {data[0].markers.map((marker, i) => { 
+                        //debugger
+
+                        return<div>
+                            
+                        <Marker 
                             icon={runner}
                             key = {marker.id} 
                             position={[
                                 marker.latitude, 
                                 marker.longitude
                             ]}
+                            routeWhileDragging={true}
                           
                             draggable={true}>
                             
@@ -100,10 +113,38 @@ return(
                                 Distance From Previous: {marker.distance_from_prev}
                                 </span>
                             </Popup>
+                            
                     
                             </Marker>
+                            {marker.add_polyline === null?
+                                null:
+                            // returnPolylines(marker)?
+                            //     null:
+                                
+
+                             <Polyline key={1} 
+                             
+                             positions={[
+                                
+                                //start 
+                                [data[0].markers[i-1].latitude,
+                                data[0].markers[i-1].longitude], 
+                                //finish
+                                [marker.latitude,
+                                marker.longitude],
+                                [marker.add_polyline[0][1],
+                                    marker.add_polyline[0][0]]
+                                // returnPolylines(marker),
+                               
+                              ]} color={'red'} />}  
+                           
+                            </div>
+                      
                         })}
-              </div>
+                </div>
+                
+                
+
               
               
               
