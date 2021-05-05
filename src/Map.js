c2e8d36b46052f3261b861fb30f244461c4225ce
giuliacajati <giuliacajati@gmail.com>
2021-05-05 13:19:27 -0400

    working onmarkers

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback, useMemo, useRef} from 'react';
-import { MapContainer, TileLayer, Marker, Popup, FeatureGroup, Circle, useMap, Polyline } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, FeatureGroup, Circle, useMap, Polyline, LayersControl } from 'react-leaflet'
 import { Icon } from 'leaflet';
 import { EditControl } from 'react-leaflet-draw';
 import { GeoSearchControl, OpenStreetMapProvider, getLatLng } from "leaflet-geosearch";
@@ -11,40 +11,22 @@ const center = {
   }
 
 const Map = () => {
-    const [draggable, setDraggable] = useState(true)
-    const [data, setData] = useState()
+    const [ draggable, setDraggable ] = useState(false)
+    const [ markerID, setSelectedMarker ] = useState(null)
+    const [ data, setData ] = useState()
     const markerRef = useRef(null)
-    const [position, setPosition] = useState(center)
+    // const [position, setPosition] = useState(center)
     const runner = new Icon({
         iconUrl:'https://upload.wikimedia.org/wikipedia/commons/b/b0/Running_icon_-_Noun_Project_17825.svg',
         iconSize: [25, 25]
     })
 
-    const eventHandlers = useMemo(
-        () => ({
-          dragend() {debugger
-              
-            const marker = markerRef.current
-            if (marker != null) {
-                debugger
-              setPosition(marker.getLatLng())
-            }
-          },
-        }),
-        [],
-      )
-      const toggleDraggable = useCallback(() => {
-        setDraggable((d) => !d)
-      }, [])
-
- 
     useEffect(() => {
-      fetch('http://localhost:3000/drawings')
-      .then(data => data.json())
-      .then(drawings => {
-        setData(drawings);
-      }) 
-      
+        fetch('http://localhost:3000/drawings')
+        .then(data => data.json())
+        .then(drawings => {
+          setData(drawings);
+        })  
     },[])
 
     function LeafletgeoSearch() {
@@ -55,16 +37,49 @@ const Map = () => {
       
           const searchControl = new GeoSearchControl({
             provider,
-            // marker: {
-            //     runner
-            // }
+            // marker: { runner }
           });
           map.addControl(searchControl);
           return () => map.removeControl(searchControl);
         }, []);
       
         return null;
+    }
+
+
+    const eventHandlers = useMemo(
+        () => ({
+          dragend() {
+            debugger
+            const marker = markerRef.current
+            if (marker != null) {
+            debugger
+              setPosition(markerID, marker.getLatLng())
+            }
+          },
+        }),
+        [],
+      )
+      const toggleDraggable = useCallback(() => {
+        setDraggable((d) => !d)
+      }, [])
+
+      const setPosition = (markerId, updatedCoordinates) => {
+        fetch(`http://localhost:3000/markers/${markerId}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+            coordinates: updatedCoordinates 
+            })
+        })
+            .then(res => res.json())   
       }
+      
+
+
       
 
 return(
@@ -74,8 +89,12 @@ return(
          <div>
              
              <MapContainer center={[38.9072, -77.0369]} zoom={15} stroke={true}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            
                
+                {/* B&W: https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png */}
                 <LeafletgeoSearch />
+                
                 <FeatureGroup>
                     <EditControl
                     position='topright'
@@ -83,23 +102,13 @@ return(
                         icon: runner,
                         draggable: true
                     }}
-                    
-                    draw={{
-                        rectangle: false
-                    }}
+                    draw={{ rectangle: false }}
                     />
-                    
-                 
                     <Circle center={[51.51, -0.06]} radius={200} />
                 </FeatureGroup>
                 
-                <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-                  {/* getLatLng() */}
-    {/* moveend */}
                 
+ 
                 <div>
                     {data[0].markers.map((marker, i) => { 
                         
@@ -112,17 +121,26 @@ return(
                              positions={ marker.add_polyline } color={'blue'} />}
                                 
                                 <Marker 
-                                    ref={markerRef}
-                                    eventHandlers={eventHandlers}
-                                    icon={runner}
-                                    key = {marker.id} 
-                                    position={[
-                                        marker.latitude, 
-                                        marker.longitude
-                                    ]}
+                                    ref={ markerRef }
+                                    eventHandlers={   eventHandlers, 
+                                                    { click: (e) => {
+                                                        setSelectedMarker(marker.id)
+                                                        if (markerID === marker.id) {
+                                                            setDraggable(true)
+                                                            console.log(eventHandlers) 
+                                                        } else {
+                                                            setDraggable(false)
+                                                        }
+                                                    }}
+                                                   }
+                                    // eventHandlers={ eventHandlers }
+                                   
+                                    icon={ runner }
+                                    key = { marker.id } 
+                                    position={ [ marker.latitude, marker.longitude ] }
                                     routeWhileDragging={true}
-                                    draggable={draggable}        
-                                                  
+                                    draggable = { draggable } 
+                                    
                                     >
                     
                                     <Popup minWidth={90}>
